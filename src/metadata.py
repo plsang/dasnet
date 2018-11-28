ac5a8c4c048f9762394274817f95593ce86215a1
plsang <phanlesang.code@gmail.com>
2018-11-28 16:29:25 +0900

    generate training lists for cityscapes and bdd dataset

--- src/metadata.py
+++ src/metadata.py
@@ -1,57 +1,116 @@
 import argparse
 import os
 import json
-from sklearn.model_selection import StratifiedShuffleSplit
 from datetime import datetime
+import glob
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def get_imageid(s):
-    """Get image id from the raw_file path,
-    e.g., clips/0313-1/8420/20.jpg'
-    return: 0313-1-8420
-    """
-    p = s.split('/')
-    img_id = p[1] + '-' + p[2]
-    return img_id
+def cityscapes_get_files(data_root, data_type, split):
+    # A map from data type to folder name that saves the data.
+    _FOLDERS_MAP = {
+        'image': 'leftImg8bit',
+        'label': 'gtFine',
+    }
+
+    # A map from data type to filename postfix.
+    _POSTFIX_MAP = {
+        'image': '_leftImg8bit',
+        'label': '_gtFine_labelIds',
+    }
+
+    # A map from data type to data format.
+    _DATA_FORMAT_MAP = {
+        'image': 'png',
+        'label': 'png',
+    }
+
+    pattern = '*%s.%s' % (_POSTFIX_MAP[data_type], _DATA_FORMAT_MAP[data_type])
+    search_files = os.path.join(
+              data_root, _FOLDERS_MAP[data_type], split, '*', pattern)
+    logger.info('Searching files with pattern: %s', pattern)
+    filenames = glob.glob(search_files)
+    return sorted(filenames)
 
 
-def to_json(lines):
-    """Convert list of json to json format
-    """
-    imgs = {}
-    for l in lines:
-        img_info = json.loads(l)
-        img_id = get_imageid(img_info['raw_file'])
-        if img_id in imgs:
-            logger.error('Duplicated key %s', img_id)
-        else:
-            imgs[img_id] = img_info
+def cityscapes(output_file, data_root, dataset_split):
+    image_files = cityscapes_get_files(data_root, 'image', dataset_split)
+    label_files = cityscapes_get_files(data_root, 'label', dataset_split)
 
-    return imgs
+    with open(output_file, 'w') as of:
+        for image_file, label_file in zip(image_files, label_files):
+            of.write('{}\t{}\n'.format(image_file, label_file))
 
+
+def bdd(output_file, data_root, dataset_split):
+
+    _LABEL_MAP = {
+        'train': 'labels_new/bdd100k_labels_images_train.json',
+        'val': 'labels_new/bdd100k_labels_images_val.json',
+    }
+
+    # A map from data type to filename postfix.
+    _POSTFIX_MAP = {
+        'image': '',
+        'label': '_drivable_id',
+    }
+
+    # A map from data type to data format.
+    _DATA_FORMAT_MAP = {
+        'image': 'jpg',
+        'label': 'png',
+    }
+
+    if dataset_split not in _LABEL_MAP:
+        logger.warning('{} split data not found'.format(dataset_split))
+        return
+
+    json_file = os.path.join(data_root, _LABEL_MAP[dataset_split])
+    logger.info('Loading %s', json_file)
+    json_data = json.load(open(json_file))
+    ids = [os.path.splitext(i['name'])[0] for i in json_data]
+
+    image_files = [os.path.join(data_root, 'images', '100k', dataset_split, \
+                                id + _POSTFIX_MAP['image'] + '.' + \
+                                _DATA_FORMAT_MAP['image']) for id in ids]
+
+    label_files = [os.path.join(data_root, 'drivable_maps', 'labels', dataset_split, \
+                                id + _POSTFIX_MAP['label'] + '.' + \
+                                _DATA_FORMAT_MAP['label']) for id in ids]
+
+    with open(output_file, 'w') as of:
+        for image_file, label_file in zip(image_files, label_files):
+            of.write('{}\t{}\n'.format(image_file, label_file))
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        '--input_dir',
-        type=str,
-        help='Path to the dataset directory'
-    )
-    parser.add_argument(
         '--output_file',
         type=str,
-        help='Path to the output file'
+        help='path to the output file'
     )
     parser.add_argument(
-        '--val_size',
-        type=int,
-        default=0.1,
-        help='percentage of training image to be used as val data'
+        '--data_root',
+        type=str,
+        help='path to the root directory containing images and labels'
+    )
+    parser.add_argument(
+        '--split',
+        type=str,
+        default='train',
+        choices=['train', 'val', 'test'],
+        help='path to the root directory containing images and labels'
+    )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        default='cityscapes',
+        choices=['cityscapes', 'bdd'],
+        help='dataset name'
     )
 
     args = parser.parse_args()
@@ -68,50 +127,10 @@ if __name__ == '__main__':
 
     start = datetime.now()
 
-    trainval_label_files = ['label_data_0313.json',
-                            'label_data_0531.json',
-                            'label_data_0601.json']
+    if args.dataset == 'cityscapes':
+        cityscapes(args.output_file, args.data_root, args.split)
+    elif args.dataset == 'bdd':
+        bdd(args.output_file, args.data_root, args.split)
 
-    test_label_file = 'test_tasks_0627.json'
-
-    trainval_lines = []
-    trainval_fileids = []
-    n_train_images = 0
-    for i, f in enumerate(trainval_label_files):
-        label_file = os.path.join(args.input_dir, f)
-        lines = [l for l in open(label_file, 'rb')]
-        logger.info('Loaded %s images', len(lines))
-        n_train_images += len(lines)
-        trainval_lines.extend(lines)
-
-        y = [i]*len(lines)
-        trainval_fileids.extend(y)
-
-    logger.info('Loaded %s training images', n_train_images)
-
-    # this is to make sure val data is stratified over all annotation files
-    sss = StratifiedShuffleSplit(
-        n_splits=1,
-        test_size=args.val_size,
-        random_state=0)
-    train_index, val_index = next(sss.split(trainval_lines, trainval_fileids))
-
-    train_lines = [trainval_lines[i] for i in train_index]
-    val_lines = [trainval_lines[i] for i in val_index]
-
-    test_label_file = os.path.join(args.input_dir, test_label_file)
-    test_lines = [l for l in open(test_label_file, 'rb')]
-    logger.info('Loaded %s test images', len(test_lines))
-
-    out = {}
-    out['train'] = to_json(train_lines)
-    out['val'] = to_json(val_lines)
-    out['test'] = to_json(test_lines)
-
-    output_dir = os.path.dirname(args.output_file)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    json.dump(out, open(args.output_file, 'w'))
     logger.info('Saved output to %s', args.output_file)
     logger.info('Time: %s', datetime.now() - start)
